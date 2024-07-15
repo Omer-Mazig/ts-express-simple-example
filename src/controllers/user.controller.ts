0cd7f2a934acb499aff1ab6ca6e902bcc5257be7
Omer Mazig <omazig93@gmail.com>
2024-07-15 11:17:41 +0300

    add mongoose

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
 
 export const getUsers = async (req: Request, res: Response) => {
+  const { query } = req;
   try {
-    // getting users from the database
-    res.json({ message: "Get all users" });
+    const users = await User.find({ name: { $regex: query.name } });
+    users[0].name;
+    res.json({ message: "Get all users 12233r3dsf" });
   } catch (error) {
     const err = error as Error;
     res.status(500).json({ error: err.message });
--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -1,5 +1,6 @@
-export interface User {
-  id: string;
+export interface IUser {
+  _id: string;
   name: string;
   email: string;
+  baba: number;
 }
